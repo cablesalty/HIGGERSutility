d42b6225943888ee92f04ca5721dd4412bf904d8
cablesalty <cablesalty@gmail.com>
2024-06-01 20:42:07 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+
+print("HIGGERSutility - HiggUtil - Version 1.0 'Cold Mountains' - cablesalty")
+
+print("""
+_   _ ___ ____  ____ _____ ____  ____        _   _ _ _ _         
+| | | |_ _/ ___|/ ___| ____|  _ \\/ ___| _   _| |_(_) (_) |_ _   _ 
+| |_| || | |  _| |  _|  _| | |_) \\___ \\| | | | __| | | | __| | | |
+|  _  || | |_| | |_| | |___|  _ < ___) | |_| | |_| | | | |_| |_| |
+|_| |_|___\\____|\\____|_____|_| \\_\\____/ \\__,_|\\__|_|_|_|\\__|\\__, |
+                                                             |___/
+""")
+print("Request spammer with UA changer")
+print("Made by: cablesalty // Not for illegal use!")
+print()
+
+while True:
+    cmd = input("HiggUtil>> ")
